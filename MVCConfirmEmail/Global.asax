8cc6e14218ce68584545342108a33c6e2c9d4476
ivanginov7 <ivanginov7@gmail.com>
2017-08-20 07:03:49 +0300

    Add project files.

---
+++ MVCConfirmEmail/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCConfirmEmail.MvcApplication" Language="C#" %>
---
+++ MVCConfirmEmail/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCConfirmEmail.Startup))]
+namespace MVCConfirmEmail
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
